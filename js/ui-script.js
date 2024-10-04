afbb5907c1c65ebb96a570e2924dfa365bcb0f68
Léo <leogianfagna@gmail.com>
2024-10-04 10:43:35 -0300

    Reformatar lista antes de gerar times

--- js/ui-script.js
+++ js/ui-script.js
@@ -1,8 +1,9 @@
 function generateTeamsButtonAction() {
+    setCleanPlayers();
     const namesInput = document.getElementById('names-input').value;
     const rulesInput = document.getElementById('rules-input').value;
     const teamCount = parseInt(document.getElementById('team-count-select').value, 10);
-    const players = sanitizePlayers(namesInput);
+    const players = namesInput.split('\n').map(name => name.trim());
     const rules = rulesInput.split('\n').map(rule => rule.split(",").map(player => player.trim()));
 
     try {
@@ -17,10 +18,12 @@ function generateTeamsButtonAction() {
         displayOnlyTeamResults(teamCount);
     } catch (e) {
         displayOnlyErrorMessage();
+        console.log(e);
     }
     window.scrollTo(0, 0);
 }
 
+// Deprecated
 function sanitizePlayers(playersString) {
     const pattern = "(?:\\d{1,2}\\s*-\\s*(?:(?:((?:[A-zÀ-ú]+\\s*[0-9]?\\s*)+))❌\\s*)?(?:(?:((?:[A-zÀ-ú]+\\s*[0-9]?\\s*)+))✅?))|(?:(?:((?:[A-zÀ-ú]+\\s*[0-9]?\\s*)+)))";
 
@@ -71,4 +74,32 @@ function copyToClipboard() {
             document.getElementById('copy-feedback-message').textContent = "Erro ao copiar os times.";
             console.error('Erro ao copiar para o clipboard: ', err);
         });
+}
+
+function setCleanPlayers() {
+    let namesInput = document.getElementById('names-input').value;
+    let namesInputTextArea = document.getElementById('names-input');
+    let jogadoresCorrigido;
+
+    const regexHeader = /^.*mensalista não confirmado:/s;
+    jogadoresCorrigido = namesInput.replace(regexHeader, '');
+    
+    let linhas = jogadoresCorrigido.split('\n');
+    let linhasFiltradas = linhas.filter(linha => linha.trim() !== '' && !linha.includes('🧤'));
+    let linhasProcessadas = linhasFiltradas.map(linha => {
+        linha = linha.replace(/^\d+\s*-\s*/, '');
+
+        // Considerar avulsos
+        if (linha.includes('❌')) {
+            let partes = linha.split('❌');
+            linha = partes[1].trim();
+        }
+
+        linha = linha.replace(/✅/g, '').trim();
+
+        return linha;
+    });
+
+    jogadoresCorrigido = linhasProcessadas.join('\n');
+    namesInputTextArea.value = jogadoresCorrigido;
 }
